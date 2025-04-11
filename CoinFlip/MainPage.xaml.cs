f032445f92d3999718266a0a59120eea41bee8af
etechas <etechas@LI08-09>
2025-04-11 20:37:25 -0300

    Add project files.

---
+++ CoinFlip/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace CoinFlip
+{
+    public partial class MainPage : ContentPage
+    {
+        // Assume que você tenha uma imagem chamada "cara.jpg" e "coroa.jpg" na pasta de recursos do seu projeto
+        // private Image MoedaImagem;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void FlipButton_Clicked(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int sorteio = random.Next(2);
+            //Animação();
+            if (sorteio == 0)
+            {
+                MoedaImagem.Source = "cara.jpg";
+            }
+            else
+            {
+                MoedaImagem.Source = "coroa.jpg";
+            }
+
+            if (SelecaoPiker.SelectedIndex == sorteio)
+            {
+                DisplayAlert("Parabéns", "Você venceu!", "Ok");
+            }
+            else
+            {
+                DisplayAlert("Tente novamente", "Você perdeu!", "Ok");
+            }
+        }
+    }
+
+}
