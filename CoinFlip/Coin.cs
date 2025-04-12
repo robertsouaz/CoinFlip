551e283cdab05567a259b095b91c91b48f085e67
roberto <robertsouaz@hotmail.com>
2025-04-11 22:37:46 -0300

    #Coin inserido
    Add Coin class and update MainPage for coin flip feature
    
    Created Coin.cs with Coin class in CoinFlip namespace, including
    a Flip method to simulate coin flips. Updated MainPage.xaml to
    properly close VerticalStackLayout. Enhanced MainPage.xaml.cs
    with FlipButton_Clicked event handler to simulate coin flips,
    update image source, and track victories and defeats.

---
+++ CoinFlip/Coin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinFlip
+{
+    public class Coin
+    {
+        private string ladoSorteado;
+
+        public string LadoSorteado { get => ladoSorteado; set => ladoSorteado = value; }
+        public Coin()
+        { // construtor não pode o mesmo nome da classe
+            
+
+        }
+        public string Flip()
+        {
+            Random random = new Random();
+            if (random.Next(2) == 0)
+            {
+                ladoSorteado = "coroa"; // valor padrão
+            }
+            else
+            {
+                ladoSorteado = "cara"; // valor padrão
+            }
+            
+            // ladoSorteado = random.Next(2) == 0 ? "cara" : "coroa"; // operador ternário
+
+            return (ladoSorteado);
+        }
+
+    }
+}
--- CoinFlip/MainPage.xaml.cs
+++ CoinFlip/MainPage.xaml.cs
@@ -5,32 +5,43 @@
         // Assume que você tenha uma imagem chamada "cara.jpg" e "coroa.jpg" na pasta de recursos do seu projeto
         // private Image MoedaImagem;
 
+
+
         public MainPage()
         {
             InitializeComponent();
         }
         private void FlipButton_Clicked(object sender, EventArgs e)
         {
+            int vitorias = 0;
+            int derrotas = 0;
+
             Random random = new Random();
             int sorteio = random.Next(2);
             //Animação();
             if (sorteio == 0)
             {
+                
                 MoedaImagem.Source = "cara.jpg";
             }
             else
             {
+                
                 MoedaImagem.Source = "coroa.jpg";
             }
 
             if (SelecaoPiker.SelectedIndex == sorteio)
             {
-                DisplayAlert("Parabéns", "Você venceu!", "Ok");
+                vitorias++;
+                DisplayAlert("Parabéns", $"Você venceu:{vitorias}", "Ok");
             }
             else
             {
-                DisplayAlert("Tente novamente", "Você perdeu!", "Ok");
+                derrotas++;
+                DisplayAlert("Tente novamente", $"Você perdeu: {derrotas}", "Ok");
             }
+
+            
         }
     }
 
